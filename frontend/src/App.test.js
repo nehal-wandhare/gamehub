38a8b92e466bc81b6b97614a07d3f31c642c055f
Nehal Wandhare <nehalwandhare000@gmail.com>
2026-04-21 16:08:12 +0530

    Update App.test.js

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app content', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  // 👇 change this text according to your UI
+  const element = screen.getByText(/game/i);
+
+  expect(element).toBeInTheDocument();
 });
